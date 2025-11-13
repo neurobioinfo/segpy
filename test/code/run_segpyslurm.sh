48c1f4ca1721fe56534b67b44e5ff3c8186ce7bc
mfiorini9 <97498007+mfiorini9@users.noreply.github.com>
2025-11-13 16:52:56 -0500

    Update run_segpyslurm.sh

--- test/code/run_segpyslurm.sh
+++ test/code/run_segpyslurm.sh
@@ -1,60 +1,60 @@
-## segrun.sslurm
-To run the pipepline, you need 1) path of the pipeline (PIPELINE_HOME), 2) Working directory , 3) VCF, and 4) PED file
+## segrun.slurm
+To run the pipepline, you need 1) path of the pipeline (PIPELINE_HOME), 2) Working directory , 3) VCF, and 4) PED file.
 ```
-export PIPELINE_HOME=/lustre03/project/6004655/COMMUN/runs/samamiri/general_files/hail/segpy.slurm
-export PWD=/lustre03/project/6004655/COMMUN/runs/samamiri/general_files/hail/practiceiPSCy/run_segpy.slurm
-export VCF=/lustre03/project/6004655/COMMUN/runs/samamiri/general_files/hail/practiceiPSCy/data/VEP_iPSC.vcf
-export PED=/lustre03/project/6004655/COMMUN/runs/samamiri/general_files/hail/practiceiPSCy/data/iPSC_2.ped
+export PIPELINE_HOME=~/segpy.pip
+PWD=~/working_dir
+VCF=~/VEP_iPSC.vcf
+PED=~/iPSC_multiple.ped
 ```
 
-#### step1: Setup
-First run runs the following code to setup the pipeline, you can change the the parameters in ${PWD}/job_output/segrun.config.ini
+#### step 0: Setup
+First, run the following code to setup the pipeline. You can change the the parameters in ${PWD}/job_output/segrun.config.ini
 ```
-sh $PIPELINE_HOME/launch_pipeline.segpy.sh \
--d ${PWD} \
---steps 0
+module load apptainer/1.2.4
+bash $PIPELINE_HOME/launch_segpy.sh \
+-d $PWD \
+--steps 0 \
+--analysis_mode multiple_family \
+--jobmode slurm
 ```
+Where analysis_mode is one of single_family, multiple_family, or case-control depending on the study design.
 
 #### Step 1:  Create table matrix
-The following code, import VCF as a MatrixTable, 
+The following code imports the VCF file as a MatrixTable: 
 ```
-sh $PIPELINE_HOME/launch_pipeline.segpy.sh \
--d ${PWD} \
+bash $PIPELINE_HOME/launch_segpy.sh \
+-d $PWD \
 --steps 1 \
---vcf ${VCF}
+--vcf $VCF
 ```
 
 #### Step 2: Run the segregation 
+The following code runs the segregation analysis:
 ```
-sh $PIPELINE_HOME/launch_pipeline.segpy.sh \
--d ${PWD} \
+bash $PIPELINE_HOME/launch_segpy.sh \
+-d $PWD \
 --steps 2 \
---vcf ${VCF} \
---ped ${PED}  
-```
-
-You can run Steps 1 and 2 using 
-```
-sh $PIPELINE_HOME/launch_pipeline.segpy.sh \
--d ${PWD} \
---steps 1-2 \
---vcf ${VCF} \
---ped ${PED}  
+--vcf $VCF
+--ped $PED
 ```
 
 #### Step 3: Clean final data
+The following code cleans up the final data file:
 ```
-sh $PIPELINE_HOME/launch_pipeline.segpy.sh \
--d ${PWD} \
---steps 3
+bash $PIPELINE_HOME/launch_segpy.sh \
+-d $PWD \
+--steps 3 \
+--parser general
 ```
+Where parser is one of general or unique.
 
 ### Note
 You can easily run step 1 to 3 using the following code 
 ```
-sh $PIPELINE_HOME/launch_pipeline.segpy.sh \
--d ${PWD} \
+bash $PIPELINE_HOME/launch_segpy.sh \
+-d $PWD \
 --steps 1-3 \
---vcf ${VCF} \
---ped ${PED}
+--vcf $VCF \
+--ped $PED \
+--parser general
 ```
